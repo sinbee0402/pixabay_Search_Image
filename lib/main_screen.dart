4ccaad3b06a68598cb97c335c464b61629a80379
sinbee0402 <osb3476@gmail.com>
2023-06-28 02:47:46 +0900

    ADD search_image App

---
+++ lib/main_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final _titleTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        title: const Center(child: Text('이미지 검색 앱')),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 8),
+            TextField(
+              controller: _titleTextController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  filled: true,
+                  hintStyle: TextStyle(color: Colors.grey[800]),
+                  hintText: '내용을 입력하세요',
+                  fillColor: Colors.white70),
+            ),
+            SingleChildScrollView(
+              child: GridView.builder(
+                // itemCount: ,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 5,
+                ),
+                itemBuilder: (BuildContext context, int index) {},
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
