9ab851c4ee15ae9fe3d6b9f30e0826a465371eba
sinbee0402 <osb3476@gmail.com>
2023-06-28 15:14:57 +0900

    Update detail_screen code

--- lib/ui/detail_screen.dart
+++ lib/ui/detail_screen.dart
@@ -3,21 +3,19 @@ import 'package:search_image/data/model/pixabay.dart';
 import 'package:search_image/data/pixabayApi.dart';
 
 class DetailScreen extends StatelessWidget {
-  final Pixabay pixabay;
+  final Pixabay data;
 
-  const DetailScreen({Key? key, required this.pixabay}) : super(key: key);
+  const DetailScreen({Key? key, required this.data}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final api = PixabayApi();
-
     return Scaffold(
       appBar: AppBar(),
       body: Hero(
-        tag: '${pixabay.id}',
+        tag: '${data.id}',
         child: Center(
           child: Image.network(
-            pixabay.imgUrl,
+            data.imgUrl,
             fit: BoxFit.cover,
             width: double.infinity,
           ),
