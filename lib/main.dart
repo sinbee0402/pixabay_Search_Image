7e631405c5231dc66d21eed5e7b0644ea68ce49e
sinbee0402 <osb3476@gmail.com>
2023-06-29 21:58:20 +0900

    Move: MainScreen 파일 이동

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:search_image/ui/main_screen.dart';
+import 'package:search_image/ui/main/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
