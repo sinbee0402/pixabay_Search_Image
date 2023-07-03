775babcecc62bfa0fe452ecae8580e321ab92ab8
sinbee0402 <osb3476@gmail.com>
2023-07-03 14:26:19 +0900

    Feat: provider 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:search_image1/data/repository/pixabay_photo_repository_impl.dart';
+import 'package:search_image1/ui/main/main_screen.dart';
+import 'package:search_image1/ui/main/main_view_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MainViewModel(PixabayPhotoRepositoryImpl()),
+        )
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ,
+      home: const MainScreen(),
     );
   }
 }
