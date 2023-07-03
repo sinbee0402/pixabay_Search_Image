3479dee0ae3e2b082e2c0538f7dcb389a683faff
sinbee0402 <osb3476@gmail.com>
2023-07-03 18:11:39 +0900

    Feat: router 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:search_image2/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
     );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,20 @@
+import 'package:go_router/go_router.dart';
+import 'package:search_image2/ui/detail/detail_screen.dart';
+import 'package:search_image2/ui/main/main_screen.dart';
+
+// GoRouter configuration
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => MainScreen(),
+      routes: [
+        GoRoute(
+          path: 'detail',
+          builder: (context, state) => DetailScreen(),
+        )
+      ],
+    ),
+  ],
+);
