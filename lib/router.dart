81c299239bff43f0932884ddaeede2edd7b1c2a8
sinbee0402 <osb3476@gmail.com>
2023-07-05 16:03:29 +0900

    Feat: ADD search_image3 App

---
+++ lib/router.dart
@@ -0,0 +1,11 @@
+import 'package:go_router/go_router.dart';
+
+// GoRouter configuration
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => Screen(),
+    ),
+  ],
+);
