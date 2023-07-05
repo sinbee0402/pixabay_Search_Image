46e6abd29152af9121e20e1ba76005491c476735
sinbee0402 <osb3476@gmail.com>
2023-07-05 16:20:14 +0900

    Feat: 기본 router 작성

--- lib/router.dart
+++ lib/router.dart
@@ -1,11 +1,20 @@
 import 'package:go_router/go_router.dart';
+import 'package:search_image3/ui/detail/detail_screen.dart';
+import 'package:search_image3/ui/main/main_screen.dart';
 
 // GoRouter configuration
 final router = GoRouter(
+  initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => Screen(),
+      builder: (context, state) => MainScreen(),
+      routes: [
+        GoRoute(
+          path: 'detail',
+          builder: (context, state) => DetailScreen(),
+        )
+      ],
     ),
   ],
 );
