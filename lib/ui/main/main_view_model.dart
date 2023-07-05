f5f26e328976baf1663eee8360907474b3d1b9a7
sinbee0402 <osb3476@gmail.com>
2023-07-05 17:25:34 +0900

    Feat: ViewModel에 State 적용

--- lib/ui/main/main_view_model.dart
+++ lib/ui/main/main_view_model.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:search_image3/data/repository/pixabay_photo_repository_impl.dart';
+import 'package:search_image3/ui/main/main_state.dart';
 
 class MainViewModel with ChangeNotifier {
   final PixabayPhotoRepositoryImpl _repositoryImpl;
 
   MainViewModel(this._repositoryImpl);
 
+  MainState _state = const MainState();
+  MainState get state => _state;
+
   void fetchImages(String query) async {
-    await _repositoryImpl.getPhotos(query);
+    final photos = await _repositoryImpl.getPhotos(query);
+
+    _state = state.copyWith(
+      photos: photos,
+    );
     notifyListeners();
   }
 }
