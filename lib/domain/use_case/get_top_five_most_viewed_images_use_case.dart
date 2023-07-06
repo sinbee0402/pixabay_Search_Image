5810d860c5cc0fab97781a160da249b4e9de09fd
sinbee0402 <osb3476@gmail.com>
2023-07-07 03:54:06 +0900

    Feat: Domain_UseCase 작성 조회수 큰 순서대로 정렬, call 사용

---
+++ lib/domain/use_case/get_top_five_most_viewed_images_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:search_image4/domain/model/photo.dart';
+import 'package:search_image4/domain/repository/photo_repository.dart';
+
+class GetTopFiveMostViewedImagesUseCase {
+  final PhotoRepository _repository;
+
+  GetTopFiveMostViewedImagesUseCase(this._repository);
+
+  Future<List<Photo>> call(String query) async {
+    final photos = await _repository.getPhotos(query);
+
+    photos.sort((a, b) => -a.views.compareTo(b.views));
+
+    return photos.toList();
+  }
+}
