880514ce7f329ca41f34df7cc66a08b6ef95f323
sinbee0402 <osb3476@gmail.com>
2023-07-07 04:04:00 +0900

    Feat: Data_Repository 작성

--- lib/data/repository/pixabay_photo_repository.dart
+++ lib/data/repository/pixabay_photo_repository.dart
@@ -1,1 +1,19 @@
 // 구현
+import 'package:search_image4/data/api/pixabay_api.dart';
+import 'package:search_image4/domain/model/photo.dart';
+import 'package:search_image4/domain/repository/photo_repository.dart';
+
+class PixabayPhotoRepository implements PhotoRepository {
+  final _api = PixabayApi();
+
+  @override
+  Future<List<Photo>> getPhotos(String query) async {
+    final photos = await _api.getPixabays(query);
+
+    if (photos == null) {
+      return [];
+    }
+
+    return photos!.toList();
+  }
+}
