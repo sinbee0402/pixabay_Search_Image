f3031bb70a51e29ce37c0cc4a48ee24f0e4cda4e
sinbee0402 <osb3476@gmail.com>
2023-07-07 04:14:39 +0900

    Feat: Presentation_Detail UI 작성

--- lib/presentation/detail/detail_screen.dart
+++ lib/presentation/detail/detail_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:search_image4/domain/model/photo.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
+  final Photo photo;
+
+  const DetailScreen({
+    Key? key,
+    required this.photo,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('조회수 : ${photo.views}'),
+      ),
+      body: Hero(
+        tag: '${photo.id}',
+        child: Center(
+          child: Image.network(
+            photo.largeImageURL,
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+        ),
+      ),
+    );
   }
 }
