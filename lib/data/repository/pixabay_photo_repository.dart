f6c1074422c221f7e3872d5bb7151db6ebd26ed2
sinbee0402 <osb3476@gmail.com>
2023-07-10 23:41:14 +0900

    Feat: repository 메서드명 수정

--- lib/data/repository/pixabay_photo_repository.dart
+++ lib/data/repository/pixabay_photo_repository.dart
@@ -4,7 +4,7 @@ import 'package:search_image5/data/model/photo.dart';
 class PixabayPhotoRepository {
   final _api = PixabayApi();
 
-  Future<List<Photo>> fetch(String query) async {
+  Future<List<Photo>> getPhotos(String query) async {
     final photos = await _api.getPixabays(query);
 
     return photos.toList();
