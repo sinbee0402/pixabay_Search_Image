d9714f1aa00852f0ed9e0b22ce1fe4de405198ce
sinbee0402 <osb3476@gmail.com>
2023-07-10 23:29:42 +0900

    Feat: use_case 작성

--- lib/data/use_case/get_top_five_most_viewed_images_use_case.dart
+++ lib/data/use_case/get_top_five_most_viewed_images_use_case.dart
@@ -1,1 +1,16 @@
+import 'package:search_image5/data/model/photo.dart';
+import 'package:search_image5/data/repository/pixabay_photo_repository.dart';
 
+class GetTopFiveMostViewedImagesUseCase {
+  final PixabayPhotoRepository _repository;
+
+  GetTopFiveMostViewedImagesUseCase(this._repository);
+
+  Future<List<Photo>> call(String query) async {
+    final photos = await _repository.getPhotos(query);
+
+    photos.sort((a, b) => -a.views.compareTo(b.views));
+
+    return photos.toList();
+  }
+}
