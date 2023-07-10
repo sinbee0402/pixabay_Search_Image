2f746452fd1c475116730b903ea9372bacf84578
sinbee0402 <osb3476@gmail.com>
2023-07-10 23:26:36 +0900

    Feat: repository 작성

---
+++ lib/data/repository/pixabay_photo_repository.dart
@@ -0,0 +1,12 @@
+import 'package:search_image5/data/api/pixabay_api.dart';
+import 'package:search_image5/data/model/photo.dart';
+
+class PixabayPhotoRepository {
+  final _api = PixabayApi();
+
+  Future<List<Photo>> getPhotos(String query) async {
+    final photos = await _api.getPixabays(query);
+
+    return photos.toList();
+  }
+}
