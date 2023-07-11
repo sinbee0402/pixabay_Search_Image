e48c6ac2d4cb41379bb8e0cd7945a385dd366573
sinbee0402 <osb3476@gmail.com>
2023-07-11 20:19:19 +0900

    Feat: main_state 작성

--- lib/ui/main/main_view_model.dart
+++ lib/ui/main/main_view_model.dart
@@ -1,15 +1,18 @@
 import 'package:search_image5/data/model/photo.dart';
 import 'package:search_image5/data/use_case/get_top_five_most_viewed_images_use_case.dart';
+import 'package:search_image5/ui/main/main_state.dart';
 
 class MainViewModel {
   final GetTopFiveMostViewedImagesUseCase _getTopFiveMostViewedImagesUseCase;
 
-  List<Photo> _photos = [];
-  List<Photo> get photos => _photos;
+  MainState _state = const MainState();
+  MainState get state => _state;
 
   MainViewModel(this._getTopFiveMostViewedImagesUseCase);
 
   void fetch(String query) async {
-    _photos = await _getTopFiveMostViewedImagesUseCase(query);
+    _state = state.copyWith(
+      photos: await _getTopFiveMostViewedImagesUseCase(query),
+    );
   }
 }
