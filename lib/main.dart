5b220346dd93ddaf25cda568c57fa9c5462ac093
sinbee0402 <osb3476@gmail.com>
2023-07-11 20:38:30 +0900

    Feat: router 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:search_image5/core/router.dart';
 import 'package:search_image5/ui/main/main_screen.dart';
 
 void main() {
@@ -10,13 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MainScreen(),
     );
   }
 }
